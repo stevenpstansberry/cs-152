38b28a7af0d509edd3d6a04db009a6afe6b04d41
Steven <stevenpstansberry@gmail.com>
2024-03-09 10:29:54 -0800

    q2

--- HW/HW2/HW2.hs
+++ HW/HW2/HW2.hs
@@ -13,6 +13,15 @@ echo [] = [] -- if input list is empty, return an empty list
 echo (x:xs) = x:x:echo xs -- when list has elements, construct a new list using first element twice alongside the recursive call to echo with the tail
 
 --Question 2:
---Function wedge: element e, list -> list
--- takes in an element and  list where 
+--Function wedge: arbitrary element e, list -> list
+-- takes in an element, list and returns new list where
+-- element is inserted between conscutive elements
+
+wedge:: a -> [a] ->[a]
+wedge n [x] = [x] -- pattern for the very last element of the list
+wedge _ [] = [] -- empty list case
+wedge n (x:xs) = x:n: wedge n xs -- pattern for when list has more than one element, constuct new list with x, element e, and the recursive call to wedge
+
+
+
 
