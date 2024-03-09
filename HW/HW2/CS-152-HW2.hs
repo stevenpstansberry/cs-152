9caa5800a3bf20022e64708da80c6baa1019c2d1
Steven <stevenpstansberry@gmail.com>
2024-03-08 19:04:08 -0800

    q1

---
+++ HW/HW2/CS-152-HW2.hs
@@ -0,0 +1,12 @@
+-- CS 152
+-- Boss Mode
+-- Maryia Sakharava
+-- Steven Stansberry
+
+-- Question 1:
+-- Function echo: list -> list
+-- takes in a list of elements of artbitary type and returns new list where each
+-- element is repeated twice, maintaining order 
+
+echo:: [a] -> [a]
+
