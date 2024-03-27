24dcbae6beeb0498e20364ac13f16e3bdd0e5b94
Steven <stevenpstansberry@gmail.com>
2024-03-27 13:41:33 -0700

    retrieve

--- Midterm-2/haskell-practice-problems.hs
+++ Midterm-2/haskell-practice-problems.hs
@@ -9,3 +9,16 @@ the predicate and False otherwise
 allTrue :: (a -> Bool) -> [a] -> Bool
 allTrue p [] = True -- base case
 allTrue p (x:xs) = p x && allTrue p xs -- continue calling allTrue so long as and returns true, if and returns false, return false
+
+{-
+Task: Define a Haskell function retrieve that takes a key and a list of tuples 
+representing key value pairs. The function returns Just the first value 
+corresponding to the given key. If the key does not appear in the list of 
+tuples, the function returns Nothing.
+Please include the type declaration for the function
+-}
+
+retrieve :: Eq k => k -> [(k,v)] -> Maybe v
+retrieve _ [] = Nothing
+retrieve k ((k',v'):xs) | k == k' = Just v'
+                      | otherwise = retrieve k xs
