40af0775f2b35a163763b1708a9cf1569235f13e
Steven <stevenpstansberry@gmail.com>
2024-04-20 13:52:44 -0700

    completed part of q1

--- HW/HW4/lava2.hs
+++ HW/HW4/lava2.hs
@@ -2,26 +2,24 @@
    Team Name:          -}
 module Lava2 where -- do not remove
 
-import Tokens2  -- Make sure you run alex to generate you scanner after you modify tokens2.x
+import Tokens2 -- Make sure you run alex to generate you scanner after you modify tokens2.x
 
-
-data ParseTree = NumNode Double
-               | OpNode Char ParseTree ParseTree
-               | IdentNode String
-               | LetNode String ParseTree ParseTree
-               | FunctionNode String ParseTree  -- one formal parameter only lambda x (* 2 x)
-               | Application ParseTree ParseTree -- First ParseTree is a FunctionNode
-        deriving Show
+data ParseTree
+  = NumNode Double
+  | OpNode Char ParseTree ParseTree
+  | IdentNode String
+  | LetNode String ParseTree ParseTree
+  | FunctionNode String ParseTree -- one formal parameter only lambda x (* 2 x)
+  | Application ParseTree ParseTree -- First ParseTree is a FunctionNode
+  deriving (Show)
 
 -- Question 1 : implement the scan function below as a call to alexScanTokens
-scan :: String ->[Token]
-
-
+scan :: String -> [Token]
+scan = alexScanTokens
 
 -- Question 2: uncomment each step as you work on it
 {-
 parse :: [Token] -> ParseTree
-
 
 -- <expr> ->  OPERATOR <expr> <expr>
 --          | POSNUM
@@ -31,10 +29,8 @@ parse :: [Token] -> ParseTree
 
 expr :: [Token] -> (Maybe ParseTree, [Token])
 
-
 -- <function> -> LAMBDA  IDENTIFIER  <expr>
 function :: [Token] -> (Maybe ParseTree, [Token])
-
 
 -- <application> ->   <function> <expr>
 application :: [Token] -> (Maybe ParseTree, [Token])
@@ -42,7 +38,6 @@ application :: [Token] -> (Maybe ParseTree, [Token])
 stringToTree:: String -> ParseTree
 stringToTree = parse.scan -- for testing convenience
 -}
-
 
 -- Question 3: uncomment each step as you work on it
 
--- HW/HW4/tokens2.x
+++ HW/HW4/tokens2.x
@@ -7,7 +7,17 @@ module Tokens2  where
 -- Add your token regular expressions and associated actions below.
 tokens :-
   $white+                        ;
+  "+"     { \s -> Operator '+' }
+  "-"     { \s -> Operator '-' }
+  "*"     { \s -> Operator '*' }
+  "/"     { \s -> Operator '/' }
+  [0-9]+(\.[0-9]+)?       { \s -> PosNum (read s) }
+  "let"               { \s -> Let }
+  "lambda"            { \s -> Lambda }
+  "in"                { \s -> In }
+  [a-zA-Z_][a-zA-Z0-9_]*    { \s -> Identifier s }
 
+  --scan "+ 2 a$" this produces an error, need to fix
 
 {
 -- Each action has type :: String -> Token
