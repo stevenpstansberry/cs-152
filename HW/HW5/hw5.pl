2912d2202f1a1b662ca65ea439f6679f9754b17d
Steven Stansberry <stevenpstansberry@gmail.com>
2024-05-07 13:54:27 -0700

    Update hw5.pl

--- HW/HW5/hw5.pl
+++ HW/HW5/hw5.pl
@@ -1,6 +1,30 @@
-#knowledge 
+% Part 1
 
 
-## Part 1
+% q1: behind predicate
 
-# q1
+
+
+% q2: first predicate
+
+
+
+% q3: infront predicate
+
+
+
+% q4: next predicate
+
+next(X, Y, [X, Y|_]). % base case 1, if X immeditally precedes Y 
+next(X, Y, [Y, X|_]). % base case 2, if Y immeditally precedes X
+next(X, Y, [_|L]) :- next(X, Y, L). % recursive case, 
+
+
+
+
+
+
+
+
+
+
