ac5701f9aeef64bf0c9033fe37e94597be23997c
Steven Stansberry <stevenpstansberry@gmail.com>
2024-05-10 13:25:35 -0700

    fixed part of backtracking

--- HW/HW5/adoption.pl
+++ HW/HW5/adoption.pl
@@ -47,23 +47,25 @@ solve(RexOwner, LisaDogBreed) :-
     member(student(RexOwner, rex, _, _, _), Students),
     %What breed is the dog of lisa?
 
-    member(student(lisa, _, _, LisaDogBreed, cs), Students),
+    member(student(lisa, _, _, LisaDogBreed, _), Students), 
 
+    first(Students, student(_, _, _, poodle, _)), % the first student is adopting the poodle 1
+    behind(student(_, _, _, _, biology), student(_, _, _, _, software_engineering), Students), % bio major is somewere behind se major 2
+    behind(student(alex, _, _, _, _), student(_, _, _, _, data_science), Students), % alex is behind ds major 3
+    behind(student(_, angel, _, _, _), student(_, _, _, _, business), Students), % adopter of angel is behind bussiness major 4
+    member(student(_, fluffy, _, _, cs), Students), % the cs major is adopting fluffy 5
+    member(student(_, bear, _, _, data_science), Students), % ds major is adopting bear 6
+    member(student(zoe, _, _, beagle, _), Students), % zoe is adopting a beagle 7 
+    infront(student(_, _, purple, _, _), student(steven, _, _, _, _), Students), % purple shirt wearer is in front of steven 8
+    behind(student(sarah, _, _, _, _), student(lisa, _, _, _, _), Students), % sarah is behind lisa 9
+    behind(student(zoe, _, _, _, _), student(_, _, _, terrier, _), Students), % zoe is behind the adopter of the terrier 10
+    behind(student(sarah, _, _, _, _), student(_, _, _, labradoodle, _), Students), % sarah is behind the adopter of the labradoodle 11
+    behind(student(_, fido, _, _, _), student(_, bear, _, _, _), Students), % adopter of fido is behind adopter of bear 12
+    next(student(_, _, red, _, _), student(_, _, green, _, _), Students), % red shirt wearing is next to green shirt 13
 
-    member(student(_, fluffy, _, _, cs), Students), % the cs major is adopting fluffy
-    member(student(_, bear, _, _, data_science), Students), % ds major is adopting bear
-    member(student(zoe, _, _, beagle, _), Students), % zoe is adopting a beagle
-    member(student(steven, _, blue, _, _), Students), % steven is wearing a blue shirt
-    member(student(alex, _, _, _, business), Students), % alex is a business makor
-    member(student(_, fido, _, husky, _), Students), % fido is a husky
+    member(student(steven, _, blue, _, _), Students), % steven is wearing a blue shirt 14
+    member(student(alex, _, _, _, business), Students), % alex is a business makor 15
+    member(student(_, _, green, _, data_science), Students), % steven is wearing a blue shirt 16
+    member(student(lisa, _, _, _, cs), Students), % lisa is a cs major 17
+    member(student(_, fido, _, husky, _), Students). % fido is a husky 18
 
-    % Detailed constraints ensuring unique setup
-    first(Students, student(_, _, _, poodle, _)), % the first student is adopting the poodle
-    behind(student(_, _, _, _, biology), student(_, _, _, _, software_engineering), Students), % bio major is somewere behind se major
-    behind(student(alex, _, _, _, _), student(_, _, _, _, data_science), Students), % alex is behind ds major
-    infront(student(_, _, purple, _, _), student(steven, _, _, _, _), Students), % purple shirt wearer is in front of steven
-    behind(student(sarah, _, _, _, _), student(lisa, _, _, _, cs), Students), $ sarah is behind lisa
-    behind(student(zoe, _, _, _, _), student(_, _, _, terrier, _), Students), % zoe is behind the adopter of the ettier
-    behind(student(sarah, _, _, _, _), student(_, _, _, labradoodle, _), Students), % sarah is behind the adopter of the labradoodle
-    behind(student(_, fido, _, _, _), student(_, bear, _, _, _), Students), % adopter of fido is behind adopter of bear
-    next(student(_, _, red, _, _), student(_, _, green, _, _), Students). % red shirt wearing is next to green shirt
