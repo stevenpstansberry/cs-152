5ca778026d4be8ed71499bbb5fcbf0bcf2f87708
maryiasakharava <99642282+maryiasakharava@users.noreply.github.com>
2024-05-09 23:10:12 -0700

    Does not return false. when backtracking

--- HW/HW5/adoption.pl
+++ HW/HW5/adoption.pl
@@ -6,72 +6,54 @@
 
 % Part 1
 
+% behind predicate
+behind(X, Y, [Y|T]) :- member(X, T).
+behind(X, Y, [_|T]) :- behind(X, Y, T).
 
-% q1: behind predicate
-behind(X, Y, [Y|L]) :- member(X, L). % base case, check if Y is head and if so, check if x is in the tail of the list
-behind(X, Y, [_|L]) :- behind(X, Y, L). % recursive case
-
-
-% q2: first predicate
+% first predicate
 first([X|_], X).
 
+% infront predicate
+infront(X, Y, [X, Y|_]).
+infront(X, Y, [_|T]) :- infront(X, Y, T).
 
-% q3: infront predicate
-
-infront(X, Y, [X, Y|_]). % base case, returns true if X and Y are two consecutive elements in that order
-infront(X, Y, [_|L]) :- infront(X, Y, L). % recursive case
-
-
-% q4: next predicate
-
-next(X, Y, [X, Y|_]). % base case 1, if X immeditally precedes Y 
-next(X, Y, [Y, X|_]). % base case 2, if Y immeditally precedes X
-next(X, Y, [_|L]) :- next(X, Y, L). % recursive case, 
+% next predicate
+next(X, Y, [X, Y|_]).
+next(X, Y, [Y, X|_]).
+next(X, Y, [_|T]) :- next(X, Y, T).
 
 
 % part 2
 
-% Knowledge
-student(steven, _, _, blue, _).
-student(alex, _, _, _, business).
-student(_, bear, _, _, data_science).
-student(lisa, fluffy, _, _, cs).
-student(_, fido, husky, _, _).
+fiveStudents([_, _, _, _, _]).
 
-% solve method, still needs work 
+
+% solve method, still needs work
+% student(name, dogname, shirtcolor, dogbreed, major)
+
+
+% Revised solve method ensuring a unique solution and proper constraints application.
+
 solve(RexOwner, LisaDogBreed) :-
-    Students = [student(alex, _, _, _, _),
-            student(lisa, _, LisaDogBreed, _, _),
-            student(sarah, _, _, _, _),
-            student(steven, _, _, _, _),
-            student(zoe, _, _, _, _)],
+    fiveStudents(Students),
 
-    % Shirt Colors Knowledge
-    member(student(_, _, _, blue, _), Students),
-    member(student(_, _, _, green, _), Students),
-    member(student(_, _, _, purple, _), Students),
-    member(student(_, _, _, red, _), Students),
-    member(student(_, _, _, yellow, _), Students),
-    
-    % Dog Knowledge
-    member(student(_, angel, _, _, _), Students),
-    member(student(_, bear, _, _, _), Students),
-    member(student(_, fido, husky, _, _), Students), % fido is a husky
-    member(student(_, fluffy, _, _, _), Students),
+    % Defining each student with more intertwined and specific properties
     member(student(RexOwner, rex, _, _, _), Students),
+    member(student(lisa, _, _, LisaDogBreed, cs), Students),
+    member(student(_, fluffy, _, _, cs), Students),
+    member(student(_, bear, _, _, data_science), Students),
+    member(student(zoe, _, _, beagle, _), Students),
+    member(student(steven, _, blue, _, _), Students),
+    member(student(alex, _, _, _, business), Students),
+    member(student(_, fido, _, husky, _), Students),
 
-    % Mapping
-    member(student(steven, _, _, blue, _), Students), % steven is wearing a blue shirt
-    member(student(lisa, fluffy, _, _, cs), Students), % lisa is adopting fluffy and is a cs major 
-    member(student(sarah, _, _, _, _), Students),
-
-    % idk if i did this right
-    behind(Students, student(sarah, _, _, _, _), 
-    student(lisa, _, _, _, _)),
-
-    behind(Students, student(sarah, _, _, _, _), 
-    student(_, _, labradoodle, _, _)),
-
-    behind(Students, student(_, fido, _, _, _), 
-    student(_, bear, _, _, _)).
-
+    % Detailed constraints ensuring unique setup
+    first(Students, student(_, _, _, poodle, _)),
+    behind(student(_, _, _, _, biology), student(_, _, _, _, software_engineering), Students),
+    behind(student(alex, _, _, _, _), student(_, _, _, _, data_science), Students),
+    infront(student(_, _, purple, _, _), student(steven, _, _, _, _), Students),
+    behind(student(sarah, _, _, _, _), student(lisa, _, _, _, cs), Students),
+    behind(student(zoe, _, _, _, _), student(_, _, _, terrier, _), Students),
+    behind(student(sarah, _, _, _, _), student(_, _, _, labradoodle, _), Students),
+    behind(student(_, fido, _, _, _), student(_, bear, _, _, _), Students),
+    next(student(_, _, red, _, _), student(_, _, green, _, _), Students).
